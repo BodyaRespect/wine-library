f28a5287c8809911b2a60bfd061ef727373daaa7
unknown <bohdan.lesyk.kn.2021@lpnu.ua>
2024-06-10 16:26:58 +0300

    navbar fixes

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,7 @@ import './App.scss'
 function App() {
   return (
     <div className="App">
-      <header>
-        <NavBar />
-      </header>
+      <NavBar />
 
       <main className="main">
         <Routes>
