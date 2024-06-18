ff3943ad6b926a7a224e6a57b9308f677fea0851
Volodymyr Shepel <web.shepel@gmail.com>
2024-06-18 21:38:01 +0300

    fixed footer layout

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,43 +3,25 @@ import { Link } from 'react-router-dom'
 export const Footer = () => {
   return (
     <footer className="footer">
-      <div className="footer__content">
-        <h1 className="footer__title">
-          Become a part
-          <br />
-          {' '}
-          of our wine family!
-        </h1>
+      <div className="container">
+        <div className="footer__content">
+          <h1 className="footer__title">
+            Become a part of our wine family!
+          </h1>
 
-        <p className="footer__text">
-          At our store, we believe every bottle of wine tells a story.
-          <br />
-          {' '}
-          It's art, culture, and tradition passed down through generations.
-          <br />
-          {' '}
-          Wine unites people, creates memories, and inspires new discoveries.
-          <br />
-          {' '}
-          We carefully select only the finest wines so you can enjoy their unique flavors and aromas.
-          <br />
-          {' '}
-          Join our wine community and discover the true essence of wine culture.
-        </p>
+          <p className="footer__text">
+            At our store, we believe every bottle of wine tells a story. It's art, culture, and tradition passed down through generations. Wine unites people, creates memories, and inspires new discoveries. We carefully select only the finest wines so you can enjoy their unique flavors and aromas. Join our wine community and discover the true essence of wine culture.
+          </p>
 
-        <div className="footer__content-bottom">
-          <div className="footer__contact-group">
-            <a className="footer__contact-group-link" href="tel:+380664357155">+38 066 435 71 55</a>
-            <a className="footer__contact-group-link" href="mailto:winelibrary@gmail.com">winelibrary@gmail.com</a>
-          </div>
-
-          <div className="footer__links-group">
-            <div className="footer__right-group">
-              <Link className="footer__link" to="/">Home</Link>
-              <Link className="footer__link" to="/about-us">About us</Link>
+          <div className="footer__bottom">
+            <div className="footer__contact-group">
+              <a className="footer__link" href="tel:+380664357155">+38 066 435 71 55</a>
+              <a className="footer__link" href="mailto:winelibrary@gmail.com">winelibrary@gmail.com</a>
             </div>
 
-            <div className="footer__left-group">
+            <div className="footer__links-group">
+              <Link className="footer__link" to="/">Home</Link>
+              <Link className="footer__link" to="/about-us">About us</Link>
               <Link className="footer__link" to="/selection">Selection</Link>
               <Link className="footer__link" to="/production">Production</Link>
             </div>
