d0e23d4aefa1bd84b72f7ce8d2eccfbb56c96e1c
Bodya Respect <evilscortez@gmail.com>
2024-07-22 12:35:11 +0300

    base path fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
+import App from '@/App'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { BrowserRouter as Router } from 'react-router-dom'
 
-import App from './App'
 import store from './store/store'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
