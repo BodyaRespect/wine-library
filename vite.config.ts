b18510b5266488a4882f88ceaac39c6de195dbf4
Bodya Respect <evilscortez@gmail.com>
2024-07-22 12:44:01 +0300

    deploy fix

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,6 @@ export default defineConfig(() => ({
 
     // ...(mode === 'development' ? [mkcert()] : []),
   ],
-  base: '/',
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
